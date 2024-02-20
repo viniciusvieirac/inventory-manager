1e464299d938118384e03c536890dc61f6f8ba83
viniciusvieirac <vini.clau0@gmail.com>
2024-02-20 09:53:48 -0300

    feat: create entity Products, docker compose and application.properties

---
+++ src/main/java/com/example/inventorymanager/models/repositories/Products.java
@@ -0,0 +1,66 @@
+package com.example.inventorymanager.models.repositories;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Products {
+  @Id
+  @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private String price;
+  private String quantity;
+
+  public Products(Long id, String name, String description, String price, String quantity) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public void setPrice(String price) {
+    this.price = price;
+  }
+
+  public String getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(String quantity) {
+    this.quantity = quantity;
+  }
+}
