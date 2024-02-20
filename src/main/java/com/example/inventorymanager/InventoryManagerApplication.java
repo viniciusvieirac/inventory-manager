fb0f4bf3f175cf8ad87547e4290418c815ffd929
viniciusvieirac <vini.clau0@gmail.com>
2024-02-20 09:58:31 -0300

    feat: create producs repository

--- src/main/java/com/example/inventorymanager/InventoryManagerApplication.java
+++ src/main/java/com/example/inventorymanager/InventoryManagerApplication.java
@@ -2,8 +2,14 @@ package com.example.inventorymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.inventorymanager.models.entities")
+@EnableJpaRepositories(basePackages = "com.example.inventorymanager.models.repositories")
+@ComponentScan(basePackages = "com.example.inventorymanager")
 public class InventoryManagerApplication {
 
 	public static void main(String[] args) {
