953ecf062ea4cd43a2edc50a0e20c6c2f865de71
viniciusvieirac <vini.clau0@gmail.com>
2024-02-24 17:14:09 -0300

    feat: method update product in service and controller and route /put

--- src/main/java/com/example/inventorymanager/controller/ProductsController.java
+++ src/main/java/com/example/inventorymanager/controller/ProductsController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,4 +50,9 @@ public class ProductsController {
     return ResponseEntity.status(HttpStatus.CREATED).body(ProductsDTO.fromEntity(newProduct));
   }
 
+  @PutMapping("/{id}")
+  public ResponseEntity<ProductsDTO> updateProduct(@PathVariable Long id, @RequestBody ProductsDTO productsDTO) {
+    Products updatedProduct = productsService.updateProduct(id, productsDTO.toEntity());
+    return ResponseEntity.ok(ProductsDTO.fromEntity(updatedProduct));
+  }
 }
--- src/main/java/com/example/inventorymanager/service/ProductsService.java
+++ src/main/java/com/example/inventorymanager/service/ProductsService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.inventorymanager.exceptions.ProductsNotFoundException;
 import com.example.inventorymanager.models.entities.Products;
 import com.example.inventorymanager.models.repositories.ProductsRepository;
 
@@ -29,4 +30,25 @@ public class ProductsService {
   public Optional<Products> getProductById(Long productId) {
     return productsRepository.findById(productId);
   }
+
+  public Products updateProduct(Long productId, Products products) {
+    return productsRepository.findById(productId)
+        .map(existingProduct -> {
+          if (products.getName() != null) {
+            existingProduct.setName(products.getName());
+          }
+          if (products.getDescription() != null) {
+            existingProduct.setDescription(products.getDescription());
+          }
+          if (products.getPrice() != null) {
+            existingProduct.setPrice(products.getPrice());
+          }
+          if (products.getQuantity() != null) {
+            existingProduct.setQuantity(products.getQuantity());
+          }
+          return productsRepository.save(existingProduct);
+        })
+        .orElseThrow(() -> new ProductsNotFoundException());
+  }
+
 }
