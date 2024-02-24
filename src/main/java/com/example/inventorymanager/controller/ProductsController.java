36a062b73cb40ab94677697c379efc3d4c94bde4
viniciusvieirac <vini.clau0@gmail.com>
2024-02-24 17:01:10 -0300

    feat: create route /post products

--- src/main/java/com/example/inventorymanager/controller/ProductsController.java
+++ src/main/java/com/example/inventorymanager/controller/ProductsController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,12 @@ public class ProductsController {
   public ResponseEntity<?> getProductById(@PathVariable Long id) throws ProductsNotFoundException {
     Optional<Products> productById = productsService.getProductById(id);
     return ResponseEntity.ok(ProductsDTO.fromEntity(productById.orElseThrow(ProductsNotFoundException::new)));
+  }
+
+  @PostMapping
+  public ResponseEntity<ProductsDTO> addNewProduct(@RequestBody ProductsDTO productsDTO) {
+    Products newProduct = productsService.addNewProduct(productsDTO.toEntity());
+    return ResponseEntity.ok(ProductsDTO.fromEntity(newProduct));
   }
 
 }
